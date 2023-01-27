7bd3c0f9223380f280523e4523009794e3895a1a
Muhindo-Galien <muhindogalien512@gmail.com>
2023-01-28 02:52:38 +0300

    added footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,10 +4,10 @@ import { FaLinkedin } from 'react-icons/fa'
 const Footer = () => {
   const date = new Date().getFullYear();
   return (
-    <div className='max-w-4xl mx-auto fotter_d mb-4 flex flex-col md:flex-row justify-between text-gray-50'> 
+    <div className='max-w-4xl mx-auto fotter_d pt-4 sm:mb-8 h-20 sm:h-0 flex flex-col md:flex-row justify-center md:justify-between text-gray-50'> 
        <h2 className='text-center sm:text-center'> &copy; {date} E-pocket  </h2>
        <a href="https://www.linkedin.com/in/muhindo-galien/" target='_blank'>
-        <h4 className='text-center sm:text-center cursor-pointer flex items-center'>Designed By Galien Muhindio <FaLinkedin className='ml-1'/> </h4>
+        <h4 className='text-center cursor-pointer '>Designed By Galien Muhindio </h4>
        </a>
     </div>
   )
